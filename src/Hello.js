92a9fda9b3bc140d12ce5527084563a3ffa4041f
njora09 <noraaerni@gmail.com>
2026-02-17 15:25:38 +0100

    added weather of city as pop-up

--- src/Hello.js
+++ src/Hello.js
@@ -3,6 +3,16 @@ import axios from "axios";
 import { Audio } from "react-loader-spinner";
 
 function Hello() {
+  function handleResponse(response) {
+    alert(`The Weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+
+  let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+  let city = "London";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div>
       <h1>Hello, World!</h1>
